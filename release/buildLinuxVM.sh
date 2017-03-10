d207627dd0c7f14915e43a507f2c14661042448d
Russell Allen <mail@russell-allen.com>
2017-03-10 22:13:24 +1100

    Update docker image for 32bit Linux

--- release/buildLinuxVM.sh
+++ release/buildLinuxVM.sh
@@ -4,7 +4,6 @@ mkdir -p artifacts
 #
 #   BUILD LINUX VM
 #
-docker run -i -v ${PWD}/..:/opt/self 32bit/ubuntu:14.04 /bin/bash -c "linux32 --32bit i386 /opt/self/vm/travis-ci-buildAndTest.sh"
+docker run -i -v ${PWD}/..:/opt/self ioft/i386-ubuntu:14.04 /bin/bash -c "linux32 --32bit i386 /opt/self/vm/travis-ci-buildAndTest.sh"
 cp ../build/vm/Self artifacts/Self
 rm -rf ../build
-
