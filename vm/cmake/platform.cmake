6cc569eb2dc2fde368ef4ce367635b9419cd451e
Valery Ushakov <uwe@stderr.spb.ru>
2022-11-27 02:12:14 +0300

    Add NetBSD to platforms (cloned from Linux)

--- vm/cmake/platform.cmake
+++ vm/cmake/platform.cmake
@@ -45,6 +45,15 @@ elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
   set(platform "linux")
   set(TARGET_OS_VERSION "LINUX_VERSION")
   
+elseif(CMAKE_SYSTEM_NAME MATCHES "NetBSD")
+
+  set(platform_kind "unix")
+  set(TARGET_OS_FAMILY  "UNIX_FAMILY")
+
+  set(platform_name "NetBSD")
+  set(platform "netbsd")
+  set(TARGET_OS_VERSION "NETBSD_VERSION")
+
 elseif(CMAKE_SYSTEM_NAME MATCHES "(Solaris|SunOS)")
   
   set(platform_kind "unix")
