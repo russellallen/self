40303cccb22d5b8885dcf64e75dc94cf7a229d68
Valery Ushakov <uwe@stderr.spb.ru>
2022-11-27 03:22:48 +0300

    sig_unix.hh: Add NetBSD to the install_flags ifdef
    The ifdef probably needs to be refactored but for now just be lazy.

--- vm/src/unix/os/sig_unix.hh
+++ vm/src/unix/os/sig_unix.hh
@@ -42,7 +42,8 @@
 // So we use common code for both platforms as much as possible. -mabdelmalek 10/02
 # if  TARGET_OS_VERSION == SOLARIS_VERSION  \
   ||  TARGET_OS_VERSION ==  MACOSX_VERSION  \
-  ||  TARGET_OS_VERSION ==   LINUX_VERSION
+  ||  TARGET_OS_VERSION ==   LINUX_VERSION  \
+  ||  TARGET_OS_VERSION ==  NETBSD_VERSION
    static int install_flags() { return SA_ONSTACK | SA_SIGINFO | SA_RESTART; }
 # elif TARGET_OS_VERSION == SUNOS_VERSION
    static int install_flags() { return SA_ONSTACK; /* ! SA_INTERRUPT */ }
