4ec275f8faa07cc40cf1eb43b1ef90ac20801767
Valery Ushakov <uwe@stderr.spb.ru>
2022-11-27 15:42:44 +0300

    asm: Add NetBSD to the gas(1) #ifdef branches
    This is not right, as NetBSD can also be built with clang as the
    default compiler, but I don't have such configuration handy at the
    moment to test.
    
    In general these files really just choose between gas and clang syntax
    and the choice should be made once instead of repeating it in a
    slightly different way each time, several times in each file.

--- vm/src/i386/runtime/asmDefs_gcc_i386.hh
+++ vm/src/i386/runtime/asmDefs_gcc_i386.hh
@@ -20,7 +20,7 @@
   # define MACRO(name, ...) .macro name // __VA_ARGS__
   # define ENDMACRO .endmacro
   # define C_SYM(name) _##name
-# elif defined(__linux__) || (defined(SOLARIS) && SOLARIS)
+# elif defined(__linux__) || defined(__NetBSD__) || (defined(SOLARIS) && SOLARIS)
   # if defined(__clang__) && !defined(NO_INTEGRATED_AS_clang)
     # define MACRO(name, ...) .macro name // __VA_ARGS__
     # define ENDMACRO .endmacro
@@ -91,7 +91,7 @@ MACRO(start_exported_function, name)
   .globl C_SYM($0)
 C_SYM($0):
 
-# elif defined(__linux__) || (defined(SOLARIS) && SOLARIS)
+# elif defined(__linux__) || defined(__NetBSD__) || (defined(SOLARIS) && SOLARIS)
 
   .global C_SYM(\name)
 C_SYM(\name):
@@ -181,7 +181,7 @@ ENDMACRO
       ENDMACRO
 
 
-    # elif defined(__linux__) || (defined(SOLARIS) && SOLARIS)
+    # elif defined(__linux__) || defined(__NetBSD__) || (defined(SOLARIS) && SOLARIS)
       MACRO(jmp_reg, reg)
         jmp *\reg
       ENDMACRO
