4f7b28c5b1422f44fc3d949d2e768715981d84db
Valery Ushakov <uwe@stderr.spb.ru>
2025-11-07 21:18:37 +0300

    setup.cmake: make warning flags version specific
    Add unconditionally only the flags that MacOS X 10.5 gcc 4.2 accepts.
    Add other flags (that said 4.2 does not accept) based on the compiler
    version.  The versions were picked from inspecting the gcc docs and
    should not be too wrong, hopefully.

--- vm/cmake/setup.cmake
+++ vm/cmake/setup.cmake
@@ -51,29 +51,17 @@ endif()
 if (clang)
   list(APPEND _flags -Wabi)
 elseif(gcc)
-  list(APPEND _flags -Wabi=11)
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8.0)
+    list(APPEND _flags -Wabi=11)
+  endif()
 endif()
+
+# These warning options are accepted by the latest xcode for MacOS X
+# 10.5 with gcc 4.2.  The compiler versions for the rest of them were
+# picked from inspecting the gcc docs and should not be too wrong.
 list(APPEND _flags
   -Wall
   -Wextra
-
-  # probably ok, but needs auditing
-  -Wno-delete-non-virtual-dtor
-
-  # old use of "volatile" in lieu of attribute((noreturn))
-  -Wno-ignored-qualifiers
-
-  # TODO: just add FALLTHROUGH annotation comments
-  -Wno-implicit-fallthrough
-
-  # these are often hit or miss...
-  -Wno-maybe-uninitialized
-
-  # complains about a few cases of existing idiomatic layout
-  -Wno-misleading-indentation
-
-  # cf. -fno-delete-null-pointer-checks above
-  -Wno-nonnull
 
   # these are a pain to fix properly...
   -Wno-sign-compare
@@ -85,15 +73,46 @@ list(APPEND _flags
   # vm/src/any/parser/parser.cpp uses some literal chars not in enum
   -Wno-switch
 
-  # there are few, marked "for debugging", and they probably need to
-  # be marked volatile so that those assignments are not elided and
-  # the values are actually available to be inspected from the
-  # debugger
-  -Wno-unused-but-set-variable
-
   # mostly spammy for C++, needs a rainy day cleanup round
   -Wno-unused-parameter
 
   # needs a rainy day cleanup round
   -Wno-unused-variable
 )
+
+# NB: XXX: For now this only handles gcc.  Recent clang cannot be used
+# to compile Self b/c they insist on this != null.  Older clang
+# versions should probably be fine, but I currently don't have
+# bandwidth spelunking throgh clang docs/rtfs in search of the right
+# versions.
+macro(VersionedFlag FirstVersion)
+  if (gcc)
+    if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL ${FirstVersion})
+      list(APPEND _flags ${ARGN})
+    endif()
+  endif()
+endmacro()
+
+# probably ok, but needs auditing
+VersionedFlag(4.7 -Wno-delete-non-virtual-dtor)
+
+# old use of "volatile" in lieu of attribute((noreturn))
+VersionedFlag(4.3 -Wno-ignored-qualifiers)
+
+# TODO: just add FALLTHROUGH annotation comments
+VersionedFlag(7.0 -Wno-implicit-fallthrough)
+
+# these are often hit or miss...
+VersionedFlag(4.7 -Wno-maybe-uninitialized)
+
+# complains about a few cases of existing idiomatic layout
+VersionedFlag(6.0 -Wno-misleading-indentation)
+
+# cf. -fno-delete-null-pointer-checks above
+# (it was supported in earlier gcc versions, but not for C++)
+VersionedFlag(5.0 -Wno-nonnull)
+
+# there are few, marked "for debugging", and they probably need to be
+# marked volatile so that those assignments are not elided and the
+# values are actually available to be inspected from the debugger
+VersionedFlag(4.6 -Wno-unused-but-set-variable)
