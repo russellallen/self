0a5e3eaef16679196a50a85c206cb93b9f668e58
Russell Allen <mail@russell-allen.com>
2026-03-02 14:46:58 +1100

    Add missing print_vm_version extern to shell.hh

--- vm64/src/any/runtime/shell.hh
+++ vm64/src/any/runtime/shell.hh
@@ -1,1 +1,23 @@
-../../../../vm/src/any/runtime/shell.hh
+/* Sun-$Revision: 30.8 $ */
+
+/* Copyright 1992-2012 AUTHORS.
+   See the LICENSE file for license information. */
+
+# ifdef INTERFACE_PRAGMAS
+  # pragma interface
+# endif
+
+
+void  abortSelf();                      // handle ^C
+
+oop   evalExpressions(Scanner* scanner);
+
+oop   eval(const char* expression, const char* fn = "<an eval string>");
+
+extern const char **prog_argv;
+extern int          prog_argc;
+
+void  run_the_VM();
+
+extern char* startUpSelfFile;
+extern bool  print_vm_version;
